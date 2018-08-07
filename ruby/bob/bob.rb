c6b3f2abe7e6518a7ed77cc3691c4cdd8ee557bb
agpiermarini <piermarini.andrew@gmail.com>
2018-08-06 18:41:19 -0600

    Started bob solution

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,12 @@
+class Bob
+  def self.hey(remark)
+    if remark[-1] == "!"
+      ""
+    elsif remark[-1] == "?"
+      ""
+    elsif remark[-1] == "."
+      ""
+    else
+      "Whatever."
+  end
+end
