99a1665fad28fd9e87cd4e3874708dcb8b1dcad8
agpiermarini <piermarini.andrew@gmail.com>
2018-08-06 19:30:52 -0600

    Add bob solution

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,12 +1,26 @@
 class Bob
   def self.hey(remark)
-    if remark[-1] == "!"
-      ""
+    remark = remark.strip.chomp
+
+    if yelling?(remark)
+      return "Calm down, I know what I'm doing!" if remark[-1] == "?"
+      "Whoa, chill out!"
     elsif remark[-1] == "?"
-      ""
-    elsif remark[-1] == "."
-      ""
+      "Sure."
+    elsif remark == ""
+      "Fine. Be that way!"
     else
       "Whatever."
+    end
   end
+
+  private
+    def self.yelling?(remark)
+      remark == remark.upcase && remark != remark.downcase
+    end
+end
+
+
+module BookKeeping
+  VERSION = 2
 end
