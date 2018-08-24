ca0524ca8989a4fea9c0109c8d738295d31e98bf
agpiermarini <piermarini.andrew@gmail.com>
2018-08-24 12:22:48 -0600

    Add circular buffer solution

--- ruby/circular-buffer/circular_buffer.rb
+++ ruby/circular-buffer/circular_buffer.rb
@@ -1,8 +1,7 @@
 class CircularBuffer
   def initialize(length)
-    @buffer = Array.new(length) {{read: nil, write: nil, data: nil}}
-    @read = 0
-    @write = 0
+    @buffer = Array.new(length) {{data: nil}}
+    @read, @write = 0, 0
   end
 
   def read
@@ -11,10 +10,9 @@ class CircularBuffer
         data = @buffer[@read][:data]
         @read += 1 and return data
       else
-        @read = 0
         raise CircularBuffer::BufferEmptyException.new
       end
-    rescue
+    rescue NoMethodError
       raise CircularBuffer::BufferEmptyException.new
     end
   end
@@ -22,11 +20,33 @@ class CircularBuffer
   def write(data)
     if @buffer[@write][:data].nil?
       @buffer[@write][:data] = data
-      @write += 1
+      increment_write and return data
     else
-      raise CircularBuffer::BufferEmptyException.new
+      raise CircularBuffer::BufferFullException.new
     end
   end
 
-  class BufferEmptyException < StandardError; end
+  def write!(data)
+    return write(data) unless buffer_full?
+    @buffer[0][:data] = data
+    @buffer.insert(-1, @buffer.delete_at(0))
+    @write = @buffer.length - 1 and return data
+  end
+
+  def clear
+    @buffer.each { | element | element[:data] = nil }
+    @read, @write = 0, 0
+  end
+
+  private
+    def buffer_full?
+      !@buffer.last[:data].nil?
+    end
+
+    def increment_write
+      @write += 1 unless buffer_full?
+    end
+
+    class BufferEmptyException < StandardError; end
+    class BufferFullException < StandardError; end
 end
