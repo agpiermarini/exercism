578434a901fb665f1444023fad734e5006787a1e
agpiermarini <piermarini.andrew@gmail.com>
2018-09-11 12:30:03 -0600

    Refactoring

--- python/hamming2/hamming.py
+++ python/hamming2/hamming.py
@@ -1,9 +1,11 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands are not the same length")
-    return 0 if len(strand_a) == 0 else calculate_distance(strand_a, strand_b)
+    return calculate_distance(strand_a, strand_b)
 
 def calculate_distance(strand_a, strand_b, index = 0, length = 0):
-    if strand_a[index] != strand_b[index]:
+    if index >= len(strand_a):
+        return length
+    elif strand_a[index] != strand_b[index]:
         length += 1
-    return length if index + 1 == len(strand_a) else calculate_distance(strand_a, strand_b, index + 1, length)
+    return calculate_distance(strand_a, strand_b, index + 1, length)
