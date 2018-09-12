11d1ab512bd5200a4ecb41980a637fcc604a3342
agpiermarini <piermarini.andrew@gmail.com>
2018-09-12 17:36:47 -0600

    Refactor Dequeu#push, #pop, #shift, and #unshift. Add private support methods #set_head, #clear_list, #list_length_one? and #list_empty?

--- ruby/linked-list/linked_list.rb
+++ ruby/linked-list/linked_list.rb
@@ -5,47 +5,50 @@ class Deque
   end
 
   def push(node = @head, value)
-    if @head.nil?
-      new_node = Node.new(value)
-      @head, @tail = new_node, new_node
-    elsif node.next
-      push(node.next, value)
-    else
-      new_node = node.append(Node.new(value))
-      @tail = new_node
-    end
+    return set_head(value) if list_empty?
+    new_node = node.next ? push(node.next, value) : node.append(Node.new(value))
+    @tail = new_node
   end
 
   def pop
-    if @head == @tail
-      value, @head, @tail = @head.value, nil, nil
-    else
-      new_tail, value = @tail.previous, @tail.value
-      @tail, @tail.next = new_tail, nil
-    end
+    return clear_list if list_length_one?
+    new_tail, value = @tail.previous, @tail.value
+    @tail, @tail.next = new_tail, nil
     value
   end
 
   def shift
-    if @head == @tail
-      value, @head, @tail = @head.value, nil, nil
-    else
-      new_head, value, = @head.next, @head.value
-      @head = new_head
-    end
+    return clear_list if list_length_one?
+    new_head, value, = @head.next, @head.value
+    @head = new_head
     value
   end
 
   def unshift(value)
-    if @head.nil?
+    return set_head(value) if list_empty?
+    old_head = @head
+    new_head = Node.new(value)
+    @head, @head.next, old_head.previous = new_head, old_head, new_head
+  end
+
+  private
+    def set_head(value)
       new_node = Node.new(value)
       @head, @tail = new_node, new_node
-    else
-      old_head = @head
-      new_head = Node.new(value)
-      @head, @head.next, old_head.previous = new_head, old_head, new_head
     end
-  end
+
+    def clear_list
+      value, @head, @tail = @head.value, nil, nil
+      value
+    end
+
+    def list_length_one?
+      @head == @tail
+    end
+
+    def list_empty?
+      @head.nil?
+    end
 end
 
 class Node
@@ -60,7 +63,6 @@ class Node
 
   def append(new_node)
     self.next = new_node
-    new_node.previous = self
-    new_node
+    new_node.previous = self and return new_node
   end
 end
