516a1827c8d0ab685eb4d2edf50b41f891e16e55
agpiermarini <piermarini.andrew@gmail.com>
2018-09-13 17:52:07 -0600

    Refactor solution

--- ruby/dominoes/dominoes.rb
+++ ruby/dominoes/dominoes.rb
@@ -1,48 +1,44 @@
 class Dominoes
   def self.chain(input_dominoes)
     return special_case(input_dominoes) if input_dominoes.length <= 1
+    dominoe_permutations = calculate_dominoe_permutations(input_dominoes)
+    index_permutations = calculate_index_permutations(input_dominoes.count)
+    find_solution(dominoe_permutations, index_permutations)
+  end
 
-    input_dominoes_reverse = input_dominoes.map { | dominoe | dominoe.reverse }
+  private
+    def self.proper_chain?(chain)
+      return false if chain[0][0] != chain[-1][-1]
+      0.upto(chain.length - 2).all? do | index |
+        current_dominoe, next_dominoe = chain[index], chain[index + 1]
+        current_dominoe[-1] == next_dominoe[0]
+      end
+    end
 
-    possible_pieces = input_dominoes.zip(input_dominoes_reverse)
+    def self.special_case(dominoe)
+      return [] if dominoe.empty?
+      dominoe[0][0] == dominoe[0][-1] ? dominoe : nil
+    end
 
-    possible_permutations = possible_pieces.permutation(input_dominoes.length).to_a
+    def self.calculate_dominoe_permutations(input_dominoes)
+      dominoes_reversed = input_dominoes.map { | dominoe | dominoe.reverse }
+      possible_pieces   = input_dominoes.zip(dominoes_reversed)
+      possible_pieces.permutation(input_dominoes.count)
+    end
 
-    dominoe_selections = [0, 1].repeated_permutation(input_dominoes.length).to_a
+    def self.calculate_index_permutations(dominoe_count)
+      [0, 1].repeated_permutation(dominoe_count)
+    end
 
-
-    possible_permutations.each do | permutation |
-      dominoe_selections.each do | selection |
-        dominoe_set = []
-        selection.each_with_index do | index, i |
-          dominoe_set.push(permutation[i][index])
+    def self.find_solution(dominoe_permutations, index_permutations)
+      dominoe_permutations.each do | permutation |
+        index_permutations.each do | selections |
+          chain = selections.map.with_index { | sel, index | permutation[index][sel] }
+          return chain if proper_chain?(chain)
         end
-        return dominoe_set if correct_set?(dominoe_set)
       end
-    end
-    return nil
-  end
-
-  def self.correct_set?(dominoe_set)
-    return false if dominoe_set[0].first != dominoe_set[-1].last
-    dominoe_set.each_with_index do | dominoe, index |
-      if index <= dominoe_set.length - 2
-        next_dominoe = dominoe_set[index + 1]
-        return false if dominoe[-1] != next_dominoe[0]
-      end
-    end
-    return true
-  end
-
-  def self.special_case(input_dominoes)
-    if input_dominoes.length.zero?
-      []
-    elsif input_dominoes[0][0] == input_dominoes[0][-1]
-      input_dominoes
-    else
       nil
     end
-  end
 end
 
-module BookKeeping; VERSION = 1; end 
+module BookKeeping; VERSION = 1; end
