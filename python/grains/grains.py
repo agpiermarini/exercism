06ec9b4b8bc2bb8462b37d0e0e964d9b665fd068
agpiermarini <piermarini.andrew@gmail.com>
2018-09-17 12:46:03 -0600

    Add bitwise solution

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,13 +1,10 @@
 def on_square(integer_number):
     check_number(integer_number)
-    return reduce(lambda x, y: x * 2, range(1, integer_number + 1))
+    return 1 << (integer_number - 1)
 
 def total_after(integer_number):
     check_number(integer_number)
-    board = [1]
-    for i, value in enumerate(range(1, integer_number + 1)):
-        board.append(board[i] * 2)
-    return board[-1] - 1
+    return (1 << integer_number) -1
 
 def check_number(integer_number):
     if integer_number <= 0 or integer_number > 64:
