7747671237b6242110deb53829875ada464a0b75
agpiermarini <piermarini.andrew@gmail.com>
2018-09-23 20:05:16 -0600

    Start proverb exercise

---
+++ ruby/proverb/proverb.rb
@@ -0,0 +1,17 @@
+class Proverb
+  def Proverb.new(*args)
+    proverb = [*args].map { |word| LINES[word] }
+    proverb.push("And all for the want of a #{[*args].first}.")
+    proverb.compact.uniq.join('')
+  end
+end
+
+LINES = {
+  "shoe"     => "For want of a nail the shoe was lost.\n",
+  "horse"    => "For want of a shoe the horse was lost.\n",
+  "rider"    => "For want of a horse the rider was lost.\n",
+  "value"    => "For want of a key the value was lost.\n",
+  "message"  => "For want of a rider the message was lost.\n",
+  "battle"   => "For want of a message the battle was lost.\n",
+  "kingdom"  => "For want of a battle the kingdom was lost.\n"
+}
