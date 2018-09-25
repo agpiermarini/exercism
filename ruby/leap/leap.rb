5ae7a9fd20884492c02a3ef2297669e2488eed1f
agpiermarini <piermarini.andrew@gmail.com>
2018-09-25 12:12:03 -0600

    Refactoring

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,7 +1,5 @@
 class Year
   def self.leap?(year)
-    if year % 4 == 0
-      year % 400 == 0 || year % 100 != 0 ? true : false
-    end
+    year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)
   end
 end
