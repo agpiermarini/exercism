8a79c6fb6050bf55ef90a41eb794a6c9643e6907
agpiermarini <piermarini.andrew@gmail.com>
2018-09-25 11:16:51 -0600

    Refactor

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -6,7 +6,7 @@ class BinarySearch
   end
 
   def search_for(value, array = list, depth = 0)
-    return error { RuntimeError.new } if array.empty?
+    return error { RuntimeError.new("Value not found") } if array.empty?
     middle = array.length / 2
     if value == array[middle]
       depth += middle
@@ -23,22 +23,8 @@ class BinarySearch
 
   private
     def check_list
-      list == list.sort ? list : error { ArgumentError.new }
-      # middle = list.length/2
-      # array_sorted?(list[0...middle], list[middle], -1) { -1 }
-      # array_sorted?(list[middle + 1..-1], list[middle], 0) { 1 }
-      # list
+      error { ArgumentError.new("List not sorted") } if list != list.sort
     end
-    #
-    # def array_sorted?(array, val, index)
-    #   return true if array[index].nil?
-    #   index >= 0 ? check_val(array[index], val) : check_val(val, array[index])
-    #   array_sorted?(array, array[index], index + yield) { yield }
-    # end
-    #
-    # def check_val(val1, val2)
-    #   val1 > val2 ? true : error { ArgumentError.new }
-    # end
 
     def error; raise yield; end
 end
