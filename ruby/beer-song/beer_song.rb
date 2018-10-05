12483cefc5536bd55d7ac145876440e06d27ddbd
agpiermarini <piermarini.andrew@gmail.com>
2018-10-05 17:07:43 -0600

    Add #verses method. Further attempts to format as expected by tests before refactoring

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,21 +1,51 @@
 class BeerSong
-  def verse(start, finish = start)
-    require 'pry'; binding.pry
-    if start >= finish || start == 0
-      if start == 2
-        "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall."
-        verse(start - 1, finish)
-      elsif start == 1
-        "1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around, no more bottles of beer on the wall."
-        verse(start - 1, finish)
-      elsif start == 0
-        "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
-      elsif start == finish
-        "#{start} bottles of beer on the wall, #{start} bottles of beer. Take one down and pass it around, #{start - 1} bottles of beer on the wall."
-      else
-        "#{start} bottles of beer on the wall, #{start} bottles of beer. Take one down and pass it around, #{start - 1} bottles of beer on the wall."
-        verse(start - 1, finish)
-      end
+  def verse(start)
+    if start == 2
+<<-TEXT
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+TEXT
+    elsif start == 1
+<<-TEXT
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+TEXT
+    elsif start == 0
+<<-TEXT
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+TEXT
+    else
+<<-TEXT
+#{start} bottles of beer on the wall, #{start} bottles of beer.
+Take one down and pass it around, #{start - 1} bottles of beer on the wall.
+TEXT
     end
+  end
+
+  def verses(start, finish = start, song = "")
+    return song if start < finish
+      if start == 2
+song += <<-TEXT
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+TEXT
+      elsif start == 1
+song += <<-TEXT
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+TEXT
+      elsif start == 0
+song += <<-TEXT
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+TEXT
+      else
+song += <<-TEXT
+#{start} bottles of beer on the wall, #{start} bottles of beer.
+Take one down and pass it around, #{start - 1} bottles of beer on the wall.\n
+TEXT
+    end
+    verses(start-1, finish, song)
   end
 end
