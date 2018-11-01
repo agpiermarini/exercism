2d32c41d26320eeb9ed1a50fbca787d860448c5a
agpiermarini <piermarini.andrew@gmail.com>
2018-11-01 15:18:25 -0600

    Add catch for limits less than 2

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,6 +1,5 @@
 def sieve(limit):
-    values = range(2, limit + 1)
+    values = range(2, limit + 1) if limit > 1 else []
     for i in values:
         values = [x for x in values if i == x or x % i != 0]
-
     return values
