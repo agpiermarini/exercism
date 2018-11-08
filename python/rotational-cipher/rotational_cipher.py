618fb4fed8c6cd27559fda8fc85f3738c9207395
agpiermarini <piermarini.andrew@gmail.com>
2018-11-08 01:46:14 -0700

    Add rotational cipher solution

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,17 @@
 def rotate(text, key):
-    pass
+    new_text = ""
+    for i in text:
+        new_value = ord(i) + key
+        if 122 > ord(i) < 65:
+            new_text += i
+        elif i.isupper():
+            if new_value > 90:
+                new_text += chr(65 + (new_value - 91))
+            else:
+                new_text += chr(new_value)
+        else:
+            if new_value > 122:
+                new_text += chr(97 + (new_value - 123))
+            else:
+                new_text += chr(new_value)
+    return new_text
