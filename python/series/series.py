b00b645f4bc2c97f7682ab7a68ccec74f95d58e2
agpiermarini <piermarini.andrew@gmail.com>
2018-11-12 18:52:33 -0800

    Add series solution

---
+++ python/series/series.py
@@ -0,0 +1,12 @@
+def slices(series, length):
+    array = []
+    if length < 1 or length > len(series):
+        raise ValueError("Length must be greater than 0 and shorter than series length.")
+    for i in range(len(series)):
+        slice = series[i:i+length]
+        if len(slice) == length:
+            array.append(series[i:i+length])
+        else:
+            return array
+
+    return array
