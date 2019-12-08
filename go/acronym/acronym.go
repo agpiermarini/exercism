16f465ccc16f3ff60d1c49d42a13d6b759199233
agpiermarini <piermarini.andrew@gmail.com>
2019-12-07 19:09:22 -0800

    Add additional acronym solution

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -2,42 +2,25 @@
 package acronym
 
 import (
+	"regexp"
 	"strings"
-	"unicode"
 )
 
-// Abbreviate function returns the acronym of a string
+// Abbreviate function returns the initialism of a string
 func Abbreviate(s string) string {
-	cleanedString := CleanString(s)
-	acronym := BuildAcronym(cleanedString)
+	re := regexp.MustCompile("[\\s-]")
+	words := re.Split(s, -1)
 
-	return acronym
-}
+	var initialism strings.Builder
+	initialism.Grow(len(words))
 
-// CleanString function
-func CleanString(s string) string {
-	var b strings.Builder
-	b.Grow(len(s))
-	for _, ch := range s {
-		if unicode.IsLetter(ch) || unicode.IsSpace(ch) {
-			b.WriteRune(ch)
-		} else if ch == '-' {
-			b.WriteRune(' ')
+	for _, word := range words {
+		re := regexp.MustCompile("[A-Za-z]")
+		firstLetter := re.FindString(word)
+		if len(firstLetter) > 0 {
+			initialism.WriteString(firstLetter)
 		}
 	}
-	return b.String()
-}
 
-// BuildAcronym function
-func BuildAcronym(s string) string {
-	titleString := strings.Title(strings.ToLower(s))
-
-	var b strings.Builder
-	b.Grow(len(titleString))
-	for _, ch := range titleString {
-		if unicode.IsUpper(ch) {
-			b.WriteRune(ch)
-		}
-	}
-	return b.String()
+	return strings.ToUpper(initialism.String())
 }
