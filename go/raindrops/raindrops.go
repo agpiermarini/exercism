f780542d2058c56f308431a46319bbbf3990d87b
agpiermarini <piermarini.andrew@gmail.com>
2019-12-10 01:04:55 -0800

    Refactor

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -8,23 +8,24 @@ import (
 
 // Convert function transforms integer to a symphony of sounds
 func Convert(input int) string {
-	sounds := make(map[int]string)
-	sounds[3], sounds[5], sounds[7] = "Pling", "Plang", "Plong"
-
 	var b strings.Builder
 	b.Grow(15)
 
-	numbers := [3]int{3, 5, 7}
-	for _, num := range numbers {
-		if input%num == 0 {
-			b.WriteString(sounds[num])
-		}
+	if input%3 == 0 {
+		b.WriteString("Pling")
+	}
+
+	if input%5 == 0 {
+		b.WriteString("Plang")
+	}
+
+	if input%7 == 0 {
+		b.WriteString("Plong")
 	}
 
 	result := b.String()
-	if len(result) == 0 {
-		return strconv.Itoa(input)
+	if len(result) > 0 {
+		return result
 	}
-
-	return result
+	return strconv.Itoa(input)
 }
