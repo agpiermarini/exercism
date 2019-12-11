1b183ab4e412df02d0402b29cd55734efbcc404a
agpiermarini <piermarini.andrew@gmail.com>
2019-12-10 22:59:12 -0800

    Add Scrabble Score solution

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,44 @@
+// Package scrabble Exercism challenge
+package scrabble
+
+import "strings"
+
+// Score function calculates the scrabble score of a word
+func Score(word string) int {
+	wordUpper := strings.ToUpper(word)
+
+	var score int
+	for _, letter := range wordUpper {
+		score += dictionary[letter]
+	}
+	return score
+}
+
+var dictionary = map[rune]int{
+	'A': 1,
+	'B': 3,
+	'C': 3,
+	'D': 2,
+	'E': 1,
+	'F': 4,
+	'G': 2,
+	'H': 4,
+	'I': 1,
+	'J': 8,
+	'K': 5,
+	'L': 1,
+	'M': 3,
+	'N': 1,
+	'O': 1,
+	'P': 3,
+	'Q': 10,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'U': 1,
+	'V': 4,
+	'W': 4,
+	'X': 8,
+	'Y': 4,
+	'Z': 10,
+}
