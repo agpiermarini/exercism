ffa178930bd4e82a1623aaebc3c5b9875c4410d6
agpiermarini <piermarini.andrew@gmail.com>
2019-12-14 13:34:00 -0800

    Add Reverse String solution

---
+++ go/reverse-string/reverse_string.go
@@ -0,0 +1,12 @@
+// Package reverse Exercism challenge
+package reverse
+
+// Reverse function reverses a string
+func Reverse(word string) string {
+	r := []rune(word)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	return string(r)
+}
