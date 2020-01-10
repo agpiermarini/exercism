a90e53d07d6bce210cf9e6854911d82342ad43ef
agpiermarini <piermarini.andrew@gmail.com>
2020-01-09 20:16:11 -0800

    Refactor

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -3,18 +3,16 @@ package grains
 
 import (
 	"errors"
+	"math"
 )
 
 // Square function returns the number of grains on a chess board square
 func Square(square int) (uint64, error) {
 	if square > 0 && square <= 64 {
-		total := 1
-		for i := 2; i <= square; i++ {
-			total = total * 2
-		}
-		return uint64(total), nil
+		total := uint64(math.Pow(2, float64(square-1)))
+		return total, nil
 	}
-	return 0, errors.New("Square must be between 1 and 64, inclusive")
+	return 0, errors.New("square must be between 1 and 64, inclusive")
 }
 
 // Total function returns total of all grains on the chess board
